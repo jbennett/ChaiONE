5795a5baa6dcfd63691e66be0650ebfdae812ed3
Jonathan Bennett <jbennett@ccistudios.com>
2013-05-14 18:56:32 -0400

    Spit out results

--- firehose.php
+++ firehose.php
@@ -1,3 +1,4 @@
+<!DOCTYPE HTML>
 <?php
 // "App.Net"
 // Consume the public feed and print out a simple list of
@@ -7,7 +8,32 @@ $globalURL = 'https://alpha-api.app.net/stream/0/posts/stream/global';
 $content = file_get_contents($globalURL);
 $content = utf8_encode($content);
 $json = json_decode($content);
+$posts = $json->data;
+?>
+<html>
+<head>
+	<title>Drinking from the ADN Firehose</title>
+	<style>
+ 	/* FIXME: Move this to a stylesheet */
+ 	#wrapper {
+ 		max-width: 960px;
+ 		margin: 0 auto;
+ 		padding: 1em 10px;
+ 	}
+	</style>
+</head>
 
-echo '<pre>';
-print_r($json);
-echo '</pre>';
+<body>
+	<div id="wrapper">
+		<h1>ADN Firehose</h1>
+		<ol class="posts">
+			<? foreach ($posts as $post): ?>
+			<li class="post">
+				<div class="username" data-username="<?= $post->user->username ?>"><?= $post->user->username ?></div>
+				<div class="message"><?= $post->html ?></div> <!-- trusting ADN here... -->
+			</li>
+			<? endforeach; ?>
+		</ol>
+	</div>
+</body>
+</html>
