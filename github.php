d9953d1dde2536064db437ec984e9051c7b3484e
Jonathan Bennett <jbennett@ccistudios.com>
2013-05-14 22:09:22 -0400

    She's working

--- github.php
+++ github.php
@@ -4,8 +4,9 @@
 // contributors to Rails by month for the past 6 months. Should display top 3
 // for each months.
 
-$date = date('Y-m-d', strtotime('6 months ago'));
-$request = 'https://api.github.com/repos/rails/rails/stats/contributors';
+$startDate = strtotime('6 months ago');
+$endDate = mktime(0,0,0, date('m'), 1, date('Y'));
+$request = "https://api.github.com/repos/rails/rails/stats/contributors";
 
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $request);
@@ -15,36 +16,48 @@ $content = curl_exec($ch);
 curl_close($ch);
 
 $content = utf8_encode($content);
-$json = json_decode($content);
+$json = json_decode($content); // get json
 
 $stats = array();
-
-
 foreach ($json as $contributor) {
 	$monthly = array();
 
+	// agregate weekly commits by month
 	foreach ($contributor->weeks as $week) {
-		$month = date('m', $week->w);
+		if ($week->w < $startDate || $week->w > $endDate) {
+			continue;
+		}
+
+		$month = date('F', $week->w);
 
 		if (!isset($monthly[$month])) {
 			$monthly[$month] = 0;
 		}
 
-		$monthly[$month] += $week->c; // using commits as the measure of active
+		$monthly[$month] += $week->c; // using commits as the measure of active, might be better to do additions + deletions
 	}
 
+	// update stats.
 	foreach ($monthly as $index => $month) {
 		if (!isset($stats[$index])) {
 			$stats[$index] = array();
 		}
 
 		// for each month, insert the authors name at index specified by their commit count (FIXME: overwriting)
-		$stats[$index][$monthly[$index]] = $contributor->author->login;
+		$stats[$index][$contributor->author->login] = $monthly[$index];
 	}
 }
 
+foreach ($stats as $month => $contributors) {
+	arsort($contributors);
 
+	echo "<p>Top Contributors for $month:</p>";
+	echo "<ul>";
 
-echo '<pre>';
-print_r($stats);
-echo '</pre>';
+	foreach(array_slice($contributors, 0, 3) as $contributor => $amount) {
+		echo "<li>$contributor ($amount)</li>";
+	}
+
+	echo "</ul>";
+
+}
