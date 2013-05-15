0cc990cc78763ec2cdba7aec515af67c927b27cf
Jonathan Bennett <jbennett@ccistudios.com>
2013-05-14 23:56:35 -0400

    Stopped using Mantle, do much, not needed for this simple case

---
+++ ADN Firehose/ADN Firehose/classes/Post.h
@@ -0,0 +1,13 @@
+//
+//  Post.h
+//  ADN Firehose
+//
+//  Created by Jonathan Bennett on 2013-05-14.
+//  Copyright (c) 2013 Jonathan Bennett. All rights reserved.
+//
+
+#import "MTLModel.h"
+
+@interface Post : NSObject
+
+@end
---
+++ ADN Firehose/ADN Firehose/classes/Post.m
@@ -0,0 +1,13 @@
+//
+//  Post.m
+//  ADN Firehose
+//
+//  Created by Jonathan Bennett on 2013-05-14.
+//  Copyright (c) 2013 Jonathan Bennett. All rights reserved.
+//
+
+#import "Post.h"
+
+@implementation Post
+
+@end
